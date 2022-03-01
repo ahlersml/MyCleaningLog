6772292bcbb69f315238b359ac1fbd258d44ba0a
walla2jd <81659539+walla2jd@users.noreply.github.com>
2022-03-01 11:10:54 -0500

    Update themes

--- app/src/main/java/com/example/mycleaninglog/ui/theme/Theme.kt
+++ app/src/main/java/com/example/mycleaninglog/ui/theme/Theme.kt
@@ -1,21 +1,33 @@
 package com.example.mycleaninglog.ui.theme
 
+import androidx.compose.animation.core.animateIntOffsetAsState
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.darkColors
 import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
 
 private val DarkColorPalette = darkColors(
-    primary = Purple200,
-    primaryVariant = Purple700,
-    secondary = Teal200
+    primary = RegularBlue,
+    primaryVariant = LightBlue,
+    secondary = Tan,
+    onSecondary = Color.White,
+    background = DarkGray,
+    onBackground = Color.White,
+    surface = DarkBlue,
+    onSurface = Color.White,
 )
 
 private val LightColorPalette = lightColors(
-    primary = Purple500,
-    primaryVariant = Purple700,
-    secondary = Teal200
+    primary = RegularBlue,
+    primaryVariant = LightBlue,
+    secondary = Tan,
+    onSecondary = Color.Black,
+    background = OffWhite,
+    onBackground = Color.Black,
+    surface =  DarkBlue,
+    onSurface = Color.Black,
 
     /* Other default colors to override
     background = Color.White,
