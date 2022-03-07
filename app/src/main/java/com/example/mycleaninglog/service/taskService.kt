097640d15909824703bf1f67a7f4de620d8ece77
kirkai <kirkai@mail.uc.edu>
2022-03-06 22:07:42 -0500

    Created service class

---
+++ app/src/main/java/com/example/mycleaninglog/service/taskService.kt
@@ -0,0 +1,10 @@
+package com.example.mycleaninglog.service
+
+import com.example.mycleaninglog.dao.ITaskDAO
+import com.example.mycleaninglog.dto.Task
+
+class taskService {
+
+    suspend fun fetchTasks()
+
+}
