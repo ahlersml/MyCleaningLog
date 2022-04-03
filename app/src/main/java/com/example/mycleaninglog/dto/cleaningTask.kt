46e079b2b72d444a9d5fbee26503cd7a9b32b922
bentaohe <60310066+bentaohe@users.noreply.github.com>
2022-04-03 09:00:52 -0400

    Update cleaningTask.kt

--- app/src/main/java/com/example/mycleaninglog/dto/cleaningTask.kt
+++ app/src/main/java/com/example/mycleaninglog/dto/cleaningTask.kt
@@ -2,9 +2,9 @@ package com.example.mycleaninglog.dto
 
 import com.google.firebase.firestore.Exclude
 
-data class cleaningTask(var cleaningTaskName:String = "", var cleaningTaskId:String = "", var uniqueID:String = ""){
+data class Task(var TaskName:String = "", var TaskId:String = "", var uniqueID:String = ""){
 
     override fun toString(): String {
-        return "$cleaningTaskName"
+        return "$TaskName"
     }
 }
