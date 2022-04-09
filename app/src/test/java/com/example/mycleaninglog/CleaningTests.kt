889120215d923ed1d3e05bbb6f9a2cbcba816173
corinmanning2315 <64652673+corinmanning2315@users.noreply.github.com>
2022-04-09 01:11:11 -0400

    begin unit tests

--- app/src/test/java/com/example/mycleaninglog/CleaningTests.kt
+++ app/src/test/java/com/example/mycleaninglog/CleaningTests.kt
@@ -1,24 +1,59 @@
 package com.example.mycleaninglog
 
 import com.example.mycleaninglog.service.CleaningService
-import org.junit.Rule
-import org.junit.Test
 import org.junit.rules.TestRule
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import com.example.mycleaninglog.dto.cleaningTask
+import kotlinx.coroutines.newSingleThreadContext
+import io.mockk.MockKAnnotations
+import io.mockk.coEvery
+import io.mockk.coVerify
+import io.mockk.every
+import io.mockk.impl.annotations.MockK
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.test.resetMain
+import kotlinx.coroutines.test.setMain
+import org.junit.*
 
 class CleaningTests {
+
     @get:Rule
     var rule: TestRule = InstantTaskExecutorRule()
     lateinit var mvm:MainViewModel
-
     lateinit var cleaningService: CleaningService
+    var allTasks : List<cleaningTask>? = ArrayList<cleaningTask>()
+
+    private val mainThreadSurrogate = newSingleThreadContext("UI thread")
+
+    @MockK
+    lateinit var mockCleaningService: CleaningService
+
+    @Before
+    fun populateTasks() {
+        Dispatchers.setMain(mainThreadSurrogate)
+        MockKAnnotations.init(this)
+        mvm = MainViewModel()
+
+    }
+
+    @After
+    fun tearDown() {
+        Dispatchers.resetMain() // reset the main dispatcher to the original Main dispatcher
+        mainThreadSurrogate.close()
+    }
+
 
     @Test
-    fun successfullyAddRoom(){
-        isDataAvailable()
+    fun `given a cleaning task dto when name is Vacuum and id is VAC then name is Vacuum and id is VAC`() {
+        var cleaningTask = cleaningTask("Vacuum", "VAC")
+        Assert.assertTrue(cleaningTask.cleaningTaskName.equals("Vacuum"))
+        Assert.assertTrue(cleaningTask.cleaningTaskId.equals("VAC"))
     }
 
-    private fun isDataAvailable() {
-        mvm = MainViewModel()
+    @Test
+    fun `given a cleaning task dto when name is Vacuum and id is VAC then output is Vacuum VAC`() {
+        var cleaningTask = cleaningTask("Vacuum", "VAC")
+        Assert.assertTrue(cleaningTask.toString().equals("Vacuum VAC"))
     }
+
 }
