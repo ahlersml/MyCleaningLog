c54741d45c9c3f943c62f651a281284e9f1b6024
corinmanning2315 <64652673+corinmanning2315@users.noreply.github.com>
2022-04-09 01:58:52 -0400

    first unit test

--- app/src/test/java/com/example/mycleaninglog/CleaningTests.kt
+++ app/src/test/java/com/example/mycleaninglog/CleaningTests.kt
@@ -4,6 +4,8 @@ import com.example.mycleaninglog.service.CleaningService
 import org.junit.rules.TestRule
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import com.example.mycleaninglog.dto.cleaningTask
+import com.example.mycleaninglog.dto.myRoom
+import com.example.mycleaninglog.service.RoomService
 import kotlinx.coroutines.newSingleThreadContext
 import io.mockk.MockKAnnotations
 import io.mockk.coEvery
@@ -12,8 +14,10 @@ import io.mockk.every
 import io.mockk.impl.annotations.MockK
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.test.resetMain
+import kotlinx.coroutines.test.runTest
 import kotlinx.coroutines.test.setMain
 import org.junit.*
+import org.junit.Assert.*
 
 class CleaningTests {
 
@@ -21,7 +25,9 @@ class CleaningTests {
     var rule: TestRule = InstantTaskExecutorRule()
     lateinit var mvm:MainViewModel
     lateinit var cleaningService: CleaningService
-    var allTasks : List<cleaningTask>? = ArrayList<cleaningTask>()
+    lateinit var roomService : RoomService
+    var allMyTasks : List<cleaningTask>? = ArrayList<cleaningTask>()
+    var allMyRooms : List<myRoom>? = ArrayList<myRoom>()
 
     private val mainThreadSurrogate = newSingleThreadContext("UI thread")
 
@@ -42,18 +48,43 @@ class CleaningTests {
         mainThreadSurrogate.close()
     }
 
-
     @Test
-    fun `given a cleaning task dto when name is Vacuum and id is VAC then name is Vacuum and id is VAC`() {
-        var cleaningTask = cleaningTask("Vacuum", "VAC")
-        Assert.assertTrue(cleaningTask.cleaningTaskName.equals("Vacuum"))
-        Assert.assertTrue(cleaningTask.cleaningTaskId.equals("VAC"))
+    fun `given rooms are available when i add a room it should appear in the list`() = runTest{
+        givenRoomServiceIsInitialized()
+        whenRoomDataIsReadAndParsed()
+        thenRoomsAreAvailable()
+    }
+
+    private fun givenRoomServiceIsInitialized(){
+        roomService = RoomService()
+    }
+
+    private suspend fun whenRoomDataIsReadAndParsed(){
+        allMyRooms = roomService.fetchRooms()
+    }
+
+    private fun thenRoomsAreAvailable(){
+        assertNotNull(allMyRooms)
+        assertTrue(allMyRooms!!.isNotEmpty())
     }
 
     @Test
-    fun `given a cleaning task dto when name is Vacuum and id is VAC then output is Vacuum VAC`() {
-        var cleaningTask = cleaningTask("Vacuum", "VAC")
-        Assert.assertTrue(cleaningTask.toString().equals("Vacuum VAC"))
+    fun `given tasks are available when i add a task it should appear in the list`() = runTest{
+        givenCleaningServiceIsInitialized()
+        whenTaskDataIsReadAndParsed()
+        thenTasksAreAvailable()
     }
 
+    private fun givenCleaningServiceIsInitialized(){
+        cleaningService = CleaningService()
+    }
+
+    private suspend fun whenTaskDataIsReadAndParsed(){
+        allMyTasks = cleaningService.fetchTasks()
+    }
+
+    private fun thenTasksAreAvailable(){
+        assertNotNull(allMyTasks)
+        assertTrue(allMyTasks!!.isNotEmpty())
+    }
 }
