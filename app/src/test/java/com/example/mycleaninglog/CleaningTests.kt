7d13e715c5df21badef38d95378505cc801059fa
Yanling <zentgryg@mail.uc.edu>
2022-04-18 21:22:13 -0400

    removed JSON related packages and classes since we didn't use JSON.Add delete unit test.

--- app/src/test/java/com/example/mycleaninglog/CleaningTests.kt
+++ app/src/test/java/com/example/mycleaninglog/CleaningTests.kt
@@ -1,22 +1,10 @@
 package com.example.mycleaninglog
 
-import com.example.mycleaninglog.service.CleaningService
 import org.junit.rules.TestRule
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import com.example.mycleaninglog.dto.cleaningTask
 import com.example.mycleaninglog.dto.myRoom
-import com.example.mycleaninglog.service.RoomService
-import kotlinx.coroutines.newSingleThreadContext
-import io.mockk.MockKAnnotations
-import io.mockk.coEvery
-import io.mockk.coVerify
-import io.mockk.every
-import io.mockk.impl.annotations.MockK
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.runBlocking
-import kotlinx.coroutines.test.resetMain
 import kotlinx.coroutines.test.runTest
-import kotlinx.coroutines.test.setMain
 import org.junit.*
 import org.junit.Assert.*
 
@@ -25,37 +13,21 @@ class CleaningTests {
     @get:Rule
     var rule: TestRule = InstantTaskExecutorRule()
     lateinit var mvm:MainViewModel
-    lateinit var cleaningService: CleaningService
-    lateinit var roomService : RoomService
     var allMyTasks = ArrayList<cleaningTask>()
     var allMyRooms = ArrayList<myRoom>()
 
-    //private val mainThreadSurrogate = newSingleThreadContext("UI thread")
-
-   // @MockK
-   // lateinit var mockCleaningService: CleaningService
-
-   /*@Before
-    fun populateTasks() {
-        Dispatchers.setMain(mainThreadSurrogate)
-        MockKAnnotations.init(this)
-        mvm = MainViewModel()
-
-    }
-
-    @After
-    fun tearDown() {
-        Dispatchers.resetMain() // reset the main dispatcher to the original Main dispatcher
-        mainThreadSurrogate.close()
-    }
-*/
     @Test
-    fun `when i add a room it should appear in the list`() = runTest{
-        whenIAddRoom()
+    fun `given rooms are available when i add a room it should appear in the list`() = runTest{
+        givenRoomServiceIsInitialized()
+        whenRoomDataIsReadAndParsed()
         thenRoomsAreAvailable()
     }
 
-    private fun whenIAddRoom(){
+    private fun givenRoomServiceIsInitialized() {
+        allMyRooms = ArrayList<myRoom>()
+    }
+
+    private fun whenRoomDataIsReadAndParsed() {
         allMyRooms.add(myRoom("Bed Room"))
         allMyRooms.add(myRoom("Kitchen"))
     }
@@ -74,10 +46,45 @@ class CleaningTests {
     }
 
     @Test
-    fun `given tasks are available when i add a task it should appear in the list`() = runTest(){
+    fun `given rooms are available when i delete a room it should remove from the list`() = runTest{
+        givenRoomListIsInitialized()
+        whenRoomDataIsRemoved()
+        thenRoomRemovedFromList()
+    }
 
+    private fun givenRoomListIsInitialized() {
+        allMyRooms = ArrayList<myRoom>()
+        allMyRooms.add(myRoom("Bed Room"))
+        allMyRooms.add(myRoom("Kitchen"))
+    }
+
+    private fun whenRoomDataIsRemoved() {
+        allMyRooms.remove(myRoom("Bed Room"))
+    }
+
+    private fun thenRoomRemovedFromList() {
+        assertNotNull(allMyRooms)
+        var containsBedroom = false
+        allMyRooms?.let{
+            it.forEach{
+                if(it.myRoomName.equals("Bed Room")){
+                    containsBedroom = true
+                }
+            }
+            assertFalse(containsBedroom)
+        }
+    }
+
+
+    @Test
+    fun `given tasks are available when i add a task it should appear in the list`() = runTest(){
+        givenCleaningServiceIsInitialized()
         whenIAddCleaningTask()
         thenCleaningTaskAreAvailable()
+    }
+
+    private fun givenCleaningServiceIsInitialized() {
+        allMyTasks = ArrayList<cleaningTask>()
     }
 
     private fun whenIAddCleaningTask() {
@@ -98,4 +105,33 @@ class CleaningTests {
         }
     }
 
+    @Test
+    fun `given tasks are available when i delete a task it should remove from the cleaning task list`() = runTest{
+        givenCleaningTaskListIsInitialized()
+        whenCleaningTaskIsRemoved()
+        thenCleaningTaskIsRemovedFromList()
+    }
+
+    private fun givenCleaningTaskListIsInitialized() {
+        allMyTasks = ArrayList<cleaningTask>()
+        allMyTasks.add(cleaningTask("Vacuum Floor"))
+        allMyTasks.add(cleaningTask("Dust Room"))
+    }
+
+    private fun whenCleaningTaskIsRemoved() {
+        allMyTasks.remove(cleaningTask("Dust Room"))
+    }
+
+    private fun thenCleaningTaskIsRemovedFromList() {
+        assertNotNull(allMyTasks)
+        var containsDustRoom = false
+        allMyTasks?.let{
+            it.forEach{
+                if(it.cleaningTaskName.equals("Dust Room")){
+                    containsDustRoom = true
+                }
+            }
+            assertFalse(containsDustRoom)
+        }
+    }
 }
